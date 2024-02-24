db5eae6ac48ed77656002453d50a92d7ddd4d2f7
takeuchima0 <masato.takeuchi@sharedine.me>
2024-02-24 16:40:40 +0900

    fix gin web api server

---
+++ cmd/lambda/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/serverless_web_api/config"
+	"github.com/serverless_web_api/handler"
+)
+
+func main() {
+	gin.SetMode(config.EnvConfig.RunMode)
+
+	routersInit := handler.InitRouter()
+	readTimeout := config.EnvConfig.ReadTimeout
+	writeTimeout := config.EnvConfig.WriteTimeout
+	maxHeaderBytes := 1 << 20
+	addr := fmt.Sprintf(":%s", config.EnvConfig.Port)
+
+	server := &http.Server{
+		Addr:           addr,
+		Handler:        routersInit,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: maxHeaderBytes,
+	}
+
+	log.Default().Printf("Listening and serving HTTP on %s\n", addr)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
---
+++ config/env.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"time"
+
+	"github.com/caarlos0/env/v7"
+)
+
+var EnvConfig = envConfig{}
+
+type envConfig struct {
+	// App Configuration
+	Environment string `env:"ENVIRONMENT" envDefault:"development"`
+	Port        string `env:"PORT" envDefault:"8080"`
+
+	// Server Configuration
+	RunMode      string        `env:"GIN_MODE" envDefault:"release"`
+	ReadTimeout  time.Duration `env:"READ_TIMEOUT" envDefault:"60s"`
+	WriteTimeout time.Duration `env:"WRITE_TIMEOUT" envDefault:"60s"`
+
+	// Database Configuration
+	DBName     string `env:"DB_NAME" envDefault:"dev"`
+	DBUser     string `env:"DB_USER" envDefault:"dev"`
+	DBPassword string `env:"DB_PASSWORD" envDefault:"password0#"`
+	DBHost     string `env:"DB_HOST" envDefault:"localhost:3306"`
+}
+
+func init() {
+	if err := env.Parse(&EnvConfig); err != nil {
+		panic(err)
+	}
+}
